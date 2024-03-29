47863dcdd02424eddba1daeab5eb337c8cf29637
Helio Junior <93434324+HelioDevv@users.noreply.github.com>
2024-03-29 02:50:19 -0300

    Att updateParkingSpot

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -82,6 +82,7 @@ public class ParkingSpotController {
 		if(!parkingSpotModelOptional.isPresent()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
 		}
+		/*
 		var parkingSpotModel = parkingSpotModelOptional.get();
 		parkingSpotModel.setParkingSpotNumber(parkingSpotDto.getParkingSpotNumber());
 		parkingSpotModel.setLicensePlateCar(parkingSpotDto.getLicensePlateCar());
@@ -91,8 +92,12 @@ public class ParkingSpotController {
 		parkingSpotModel.setResponsibleName(parkingSpotDto.getResponsibleName());
 		parkingSpotModel.setApartment(parkingSpotDto.getApartment());
 		parkingSpotModel.setBlock(parkingSpotDto.getBlock());
+		*/
 		
+		var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
+        parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
 		return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
 	}
-	
 }
